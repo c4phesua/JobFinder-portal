96d71c11b33ba82f82b75aad65500faf725337b9
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-05-29 22:55:47 +0700

    implement UI for jobs

--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -4,15 +4,15 @@ export default class NavigationBar extends React.Component {
   render() {
     return (
       <>
-        <div className="navbar">
+        <header className="navbar">
           <div className="brand"><a href="/" className="brandName">Job Finder</a></div>
           <div className="search">
-            <input type="text" className="keyword"/>
-            <input type="text" className="title"/>
+            <input type="text" className="keyword" />
+            <input type="text" className="title" />
             <button title="Search" className="btnSearch">Search</button>
           </div>
           <div className="login"><a href="/login">Login</a></div>
-        </div>
+        </header>
       </>
     );
   }
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Jobs from '../component/Jobs';
 class Dashboard extends React.Component {
 
   render() {
     return (
       <>
-        
+      <Jobs />
       </>
     );
   }
