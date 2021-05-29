06ecd028e619acec91a40019329dc252eb3646cb
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-05-29 17:13:46 +0700

    modify fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import NavigationBar from './component/NavigationBar';
+import Login from './page/Login';
+import Dashboard from './page/Dashboard';
 
 
 
@@ -9,6 +11,14 @@ function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/index" component={Dashboard} />
+          <Route exact path="/index.html" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -5,18 +5,20 @@ export default class NavigationBar extends React.Component {
   render() {
     return (
       <>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">Job Finder</Navbar.Brand>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Navbar>
+        <div className="modified-navbar">
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Job Finder</Navbar.Brand>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-light">Search</Button>
+            </Form>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar>
+        </div>
       </>
     );
   }
