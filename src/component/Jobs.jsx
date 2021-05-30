6c4b4a21c67653d54b457c4e558884d38f77e4e6
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-05-30 15:59:35 +0700

    prototype job description

--- src/component/Jobs.jsx
+++ src/component/Jobs.jsx
@@ -11,6 +11,10 @@ export default class Jobs extends React.Component {
         <Job title="Java developer" index="0" company="FPT Software"/>
         <Job title="Java developer" index="1" company="Katalon Vietnam" />
         <Job title="Java developer" index="2" company="KMS Technology" />
+        <Job title="Java developer" index="3" company="KMS Technology" />
+        <Job title="Java developer" index="4" company="KMS Technology" />
+        <Job title="Java developer" index="5" company="KMS Technology" />
+        <Job title="Java developer" index="6" company="KMS Technology" />
       </div>
     );
   }
