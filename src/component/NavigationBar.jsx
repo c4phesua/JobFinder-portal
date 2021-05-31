8aa4436dae1e0dafdebd5466e5d4667e37b618c8
CaroKann <th1475369@gmail.com>
2021-05-31 07:34:20 +0700

    add material-ui-icons to dependency

--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -7,17 +7,17 @@ export default class NavigationBar extends React.Component {
 
   render() {
     return (
-      <>
-        <header className="navbar-custom">
-          <div className="brand"><a href="/" className="brandName">Job Finder</a></div>
-          <div className="search">
-            <input type="text" className="keyword" />
-            <input type="text" className="title" />
-            <button title="Search" className="btnSearch">Search</button>
-          </div>
-          <button className="login" onClick={this.handleLoginClick}>Login</button>
-        </header>
-      </>
+        <>
+          <header className="navbar-custom">
+            <div className="brand"><a href="/" className="brandName">Job Finder</a></div>
+            <div className="search">
+              <input type="text" className="keyword" />
+              <input type="text" className="title" />
+              <button title="Search" className="btnSearch">Search</button>
+            </div>
+            <button className="login" onClick={this.handleLoginClick}>Login</button>
+          </header>
+        </>
     );
   }
 }
