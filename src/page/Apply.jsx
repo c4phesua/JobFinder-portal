3a3ccd27bda7ab62e45b1bc52b4a88b5228c6b6d
Min <pc2.myn@gmail.com>
2021-06-04 21:50:41 +0700

    fix_fucntion

--- src/page/Apply.jsx
+++ src/page/Apply.jsx
@@ -5,67 +5,17 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Icon from '@material-ui/core/Icon';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import SendIcon from '@material-ui/icons/Send';
+import {useStylesApply,ColorButton} from '../utils/UtilsFunc';
 
-
-const useStyles = makeStyles((theme) => ({
-  
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 600,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(16),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-  },
-}));
-
-const ColorButton = withStyles((theme) => ({
-    root: {
-      color: theme.palette.getContrastText(red[500]),
-      backgroundColor: red[500],
-      '&:hover': {
-        backgroundColor: red[700],
-      },
-    },
-  }))(Button);
 
 
 export default function Checkout() {
-  const classes = useStyles();
+  const classes = useStylesApply();
   return (
     <React.Fragment>
       <CssBaseline />
