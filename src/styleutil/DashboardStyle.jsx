b24b3ecf25ad91b0dc16e070bd8f3cbef38a164f
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-06-15 22:28:04 +0700

    add style

---
+++ src/styleutil/DashboardStyle.jsx
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const body = document.body,
+    html = document.documentElement;
+const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+export const dashBoardStyle = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+        minWidth: '100%',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+        minHeight: height * 85 / 100,
+        maxHeight: height * 85 / 100,
+    },
+    fixedHeight: {
+        height: 240,
+    },
+}));
