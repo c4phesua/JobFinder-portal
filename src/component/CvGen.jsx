7f221a0bc3d14a016d517be9ff9f8708fcf21b1b
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-06-15 22:33:14 +0700

    refractor cvgen

--- src/component/CvGen.jsx
+++ src/component/CvGen.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Title from "./Title";
-import {Select} from "@material-ui/core";
+import { Select, TableBody, TableHead, TableCell, TableRow, Table, Link } from "@material-ui/core";
 
 // Generate Order Data
 function createData(id, date, name, email, cv, status) {
