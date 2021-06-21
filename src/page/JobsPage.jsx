6973d417963f999f9c4443336519d85b77b08c40
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-06-21 23:03:41 +0700

    commit

--- src/page/JobsPage.jsx
+++ src/page/JobsPage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import MockupData from '../helper/MockupData';
 import { Container } from '@material-ui/core';
 import Job from '../component/Job';
-import JobFilter from '../component/JobFilter';
+import ScrollToTop from '../component/ScrollToTop';
+// import JobFilter from '../component/JobFilter';
 const jobs = MockupData.data_home_page.JOB_LIST;
 
 const renderJob = (job) => {
@@ -11,25 +12,26 @@ const renderJob = (job) => {
   );
 }
 
-const renderFilter = () => {
-  return (
-    <JobFilter />
-  );
-}
+// const renderFilter = () => {
+//   return (
+//     <JobFilter />
+//   );
+// }
 
 const JobsPage = () => {
   document.title = "JobFinder - Việc làm";
 
   return (
     <Container maxWidth="lg">
-      {
+      {/* {
         renderFilter()
-      }
+      } */}
       {
         jobs.map((job) => {
           return renderJob(job);
         })
       }
+      <ScrollToTop />
     </Container>
   )
 }
