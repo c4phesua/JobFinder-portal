86d0ba1b69a620d901f878bef4d32aca08681422
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-06-22 21:14:41 +0700

    remove unused

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -6,7 +6,6 @@ import EditProfileSuggest from '../component/EditProfileSuggest';
 import ScrollToTop from '../component/ScrollToTop';
 const newJobs = MockupData.data_home_page.NEW_JOB;
 const hotJobs = MockupData.data_home_page.HOT_JOB;
-const jobs = MockupData.data_home_page.JOB_LIST;
 
 const Homepage = () => {
   document.title = "JobFinder - Trang chủ";
